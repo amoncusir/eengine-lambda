8628d3f259b94eb427606e3c1757737218e4faa1
Aran <amoncusir@gmail.com>
2019-11-04 04:17:17 +0100

    Service and Subscriber docs

--- core/src/main/kotlin/info/digitalpoet/eengine/core/subscriber/Service.kt
+++ core/src/main/kotlin/info/digitalpoet/eengine/core/subscriber/Service.kt
@@ -3,8 +3,13 @@ package info.digitalpoet.eengine.core.subscriber
 /** <!-- Documentation for: info.digitalpoet.eengine.core.subscriber.Service on 29/8/19 -->
  *
  * The services will be created when published message find your subscribers.
- *
  * Any manager represents a collection of subscribers.
+ *
+ * Each [Service] represents an work group, like all instances of the same service.
+ *
+ * This association as used to select the [Subscriber] or many [Subscriber] to send the current
+ * [info.digitalpoet.eengine.core.message.Message]. This decision will be taken by the
+ * [info.digitalpoet.eengine.core.broadcast.BroadcastHandler] class implementation.
  *
  * @author Aran Moncusí Ramírez
  */
--- core/src/main/kotlin/info/digitalpoet/eengine/core/subscriber/Subscriber.kt
+++ core/src/main/kotlin/info/digitalpoet/eengine/core/subscriber/Subscriber.kt
@@ -6,13 +6,13 @@ import info.digitalpoet.eengine.core.matcher.MessageMatcher
 /** <!-- Documentation for: info.digitalpoet.eengine.core.subscriber.Subscriber on 29/8/19 -->
  *
  * Subscriber represent any manager, function or something that can process events.
- * The method for send each event is responsibility of [Deliverer] instance.
  *
+ * *NOTE:* The method for send each event is responsibility of [Deliverer] instance.
+ * *NOTE:* The method for know if the instance of [Subscriber] can accept the message is responsibility of [MessageMatcher]
+ * instance.
  *
  * The Subscribers can be connect with others using the field [Subscriber.serviceId] that represents a [Service].
  * (_see [Service] docs for more information_].
- *
- *
  *
  * @author Aran Moncusí Ramírez
  */
